cfa30ae54ba4a87a5dfec369b4e18976b252cdbe
AbiAdam <107949232+AbiAdam@users.noreply.github.com>
2025-07-15 18:47:12 -0700

    Lidar

---
+++ code/lib/Lidar/Lidar.cpp
@@ -0,0 +1,113 @@
+#include <Arduino.h>
+#include <VL53L1X.h>
+#include <Wire.h>
+
+#include <ServoESP.h>
+#include "Lidar.h"
+
+#include <stdexcept>  // for std::runtime_error
+#include <map> // Required header for std::map
+
+// #include "Constants.h"
+// #include "PinSetup.h"
+
+
+unsigned long timeout = 1000;   // val in ms (check this)
+unsigned long timeoutLidar = 1000; 
+const int angularStep = 1; // val in degrees
+
+Lidar::Lidar(int sclPin, int sdaPin, ServoESP &servo) : sclPin(sclPin), sdaPin(sdaPin), servo(servo)
+{ 
+    // servo obj initialisation
+    servo.attach();
+
+    servo.moveServo(0); 
+
+    // lidar initialisation
+    bool initialisationSuccess = initialiseLidar();
+
+    if (!initialisationSuccess)
+    {
+        Serial.println("Failed to connect!"); 
+    }
+}
+
+/*
+ * Sweep the servo from 0-180, at each point saving distance reading to obtain map with angle --> distance to serve as the robot's vision
+ *
+ * @return map with key = angle of servo, value = distance reading taken at that point
+ *
+ * the angle increments in steps of size controlled by constants above (AND LATER IN CONSTANTS.H)
+ */
+std::map<int, uint16_t> Lidar::sweepReading(int endAngle)
+{
+    std::map<int, uint16_t> readings;
+
+    for (int angle = 0; angle <= endAngle; angle += angularStep)
+    {
+        this->servo.moveServo(angle);
+        uint16_t reading = singleMeasurement();
+        readings.insert({angle, reading});
+    }
+
+    //servo.moveServo(0); 
+
+    return readings; 
+}
+
+/*
+ * Stops measurements and the servos motion
+ *
+ */
+bool Lidar::stop() {
+    servo.moveServo(0); 
+    servo.detach();  // Or your stop logic
+    return true;
+}
+
+/* returns false if timed out */
+bool Lidar::initialiseLidar()
+{
+    Wire.begin(this->sdaPin, this->sclPin);
+    Wire.setClock(400000); // use 400 kHz I2C
+
+    unsigned long start = millis(); 
+
+    sensor.setTimeout(200); // setting to min timeout for short mode; not giving us the ability to use any others bc its the most resistant to ambient light AND it has a 1.3m sensing range, which is more than enough
+    while (!sensor.init())
+    {
+       if (millis() - start > timeoutLidar){
+            return false; 
+       } 
+    }
+
+    // Minimum timing budget is 20 ms for short distance mode and 33 ms for
+    // medium and long distance modes. See the VL53L1X datasheet for more
+    // information on range and timing limits.
+    sensor.setDistanceMode(VL53L1X::Short);
+    sensor.setMeasurementTimingBudget(20000);
+
+    return true; 
+}
+
+/*
+this returns a uint containing the measurement, if failed it returns 0 **choose different value*** --> exceptions are apparently terrible for esps
+*/
+uint16_t Lidar::singleMeasurement()
+{
+    sensor.readSingle(false); // false means non-blocking
+
+    unsigned long start = millis();
+
+    while (!sensor.dataReady())
+    { // wait until measurement ready
+        // do other stuff --> to be determined based on needs later in the project
+        if (millis() - start > timeout)
+        {
+            return 0; 
+        }
+        
+    }
+
+    return sensor.read(false); // return value
+}
---
+++ code/lib/Lidar/Lidar.h
@@ -0,0 +1,42 @@
+#ifndef LIDAR_H
+#define LIDAR_H
+
+#include <Arduino.h>
+#include <ServoESP.h>
+#include <VL53L1X.h>
+#include <cmath>
+
+// Data structure inclusions: 
+#include <map> // Required header for std::map
+
+class Lidar
+{
+public:
+    Lidar(int sclPin, int sdaPin, ServoESP& servo);
+
+    /*
+     * Obtain distance reading array --> this should spin the servo
+     */
+    std::map<int, uint16_t> sweepReading(int endAngle);
+
+    /*
+     * Stops measurements and the servos motion
+     *
+     * @param angle
+     */
+    bool stop();
+
+    // add continuous reading
+
+private:
+    VL53L1X sensor;
+    ServoESP &servo;
+    int sclPin;
+    int sdaPin; // these are i2c enabled pins for communicating with the LiDAR
+
+    bool initialiseLidar();
+
+    uint16_t singleMeasurement(); 
+};
+
+#endif
